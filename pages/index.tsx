f9319aa8956472fb794a6a846f4962e3d0e36cc5
Rob Stone <robstone62@gmail.com>
2020-11-12 13:55:05 +0100

    Attempted to update template image
    Testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ const Home = () => {
         title,
         tagline,
         cloudName: 'deaib8dmw',
-        imagePublicID: 'iea-template',
+        imagePublicID: 'iea/iea-template',
         titleFont: 'Lato',
         taglineFont: 'Lato',
         titleExtraConfig: '_bold',
